102fb017919d092f0aabbd49b650500ebd612e3e
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2022-10-14 12:30:31 +0530

    Update main.js
    updated

--- main.js
+++ main.js
@@ -12,11 +12,10 @@ app.get("/hello",(req , res) => {
 res.send("hello working")
 })
 
-app.get("/platform/twitter/user-detail-by-userid/:user", function (req, res) {
-  res.send(req.params.user)
-//   proxy.web(req, res, {
-//     target: "http://92.204.128.4:4000",
-//   });
+app.use(function (req, res) {
+  proxy.web(req, res, {
+    target: "http://92.204.128.4:4000",
+  });
 });
 app.listen(3000, () => {
   console.log("server is listening on port", 3000);
