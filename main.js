3fdbac450a09cd5d30228d28ced75f882f730053
Amitabh <32864393+MakeAnIque@users.noreply.github.com>
2022-10-14 11:45:44 +0530

    Update main.js
    Update main.js to env

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const app = require("express")();
 const cors = require("cors");
 const httpProxy = require("http-proxy");
-
+require("dotenv").config({ path: ".env" });
 const proxy = httpProxy.createProxyServer();
 //create a server object:
 app.use(cors());
+
 app.disable("x-powered-by");
 app.use(function (req, res) {
   proxy.web(req, res, {
     target: "http://92.204.128.4:4000",
   });
 });
-app.listen(8080);
+app.listen(process.env.PORT, () => {
+  console.log("server is listening on port", process.env.PORT);
+});
